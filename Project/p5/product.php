4ac485d6a308b4fd34804eefcb59680448822258
Jagdish Chudasama <chudasamajagdish12@gmail.com>
2021-09-23 18:15:48 +0530

    Add files via upload

---
+++ Project/p5/product.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>The Big Dog</title>
+<style type="text/css">
+	body{
+			background:
+			radial-gradient(black 15%, transparent 16%) 0 0,
+			radial-gradient(black 15%, transparent 16%) 8px 8px,
+			radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+			radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+			background-color:#282828;
+			background-size:16px 16px;
+		}
+</style>
+</head>
+
+<body>
+	
+</body>
+</html>
